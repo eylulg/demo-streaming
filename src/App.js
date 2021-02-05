d516dea25ed5ad09c0695148fe01ea295b3f7f7e
Eylül Güneş <eylulgist@gmail.com>
2021-02-06 00:03:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App({title}) {
+  return (
+    <div className="App">
+      <header className="App-header">{title}</header>
+    </div>
+  );
+}
+
+export default App;
